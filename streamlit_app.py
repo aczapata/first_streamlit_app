b11b4e7b2974bbd0417af1969d7c91c622c28c8e
Andrea Zapata <andreaczapata@gmail.com>
2022-07-22 10:56:18 -0500

    Update streamlit_app.py
    Add menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,11 @@
 
+import streamlit
+
+streamlit.title("My parents New Healthy Diner")
+
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥚 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑 Avocado Toast')
+streamlit.header('🥭 Fruit Smoothie 🍇')
