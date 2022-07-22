c74c22532f0c0b36f1841dafe37fc486fbdfea86
Andrea Zapata <andreaczapata@gmail.com>
2022-07-22 11:19:39 -0500

    Update streamlit_app.py
    Add fruits dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 
 import streamlit
+import pandas as pd
+
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 streamlit.title("My parents New Healthy Diner")
 
@@ -9,3 +12,5 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥚 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑 Avocado Toast')
 streamlit.header('🥭 Fruit Smoothie 🍇')
+
+streamlit.dataframe(my_fruit_list)
